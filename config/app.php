82ad5265954f7361fa34ebc9691220c95395a15c
Sithira <sithiraac@gmail.com>
2016-07-19 08:16:20 +0530

    Added Configs to App

--- config/app.php
+++ config/app.php
@@ -147,6 +147,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        CodeZero\Flash\FlashServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -201,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Flash' => CodeZero\Flash\Facade\Flash::class,
 
     ],
 
