1235a2b529e042c82bd35584462f2505a0034670
Sithira <sithiraac@gmail.com>
2016-07-30 10:27:52 +0530

    after arlms giving :P

--- app/Http/Controllers/Employee/TimeSheetController.php
+++ app/Http/Controllers/Employee/TimeSheetController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Employee;
 
+use App\Models\Employee\OverTime;
 use App\Models\Employee\TimeSheet;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -22,6 +24,18 @@ class TimeSheetController extends Controller
 
     public function checkOut(Request $request, $employee)
     {
-        // code
+        $person = User::whereId($employee)->firstOrFail();
+
+        $person->check_out = Carbon::now()->toDateString();
+
+        if (($person->check_in - $person->check_out) > 8) {
+
+            $day = Carbon::now()->toDateString();
+
+            OverTime::create([
+                'timesheet_id' => '' 
+            ]);
+        }
+
     }
 }
