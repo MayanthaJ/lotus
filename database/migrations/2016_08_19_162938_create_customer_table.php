88d3d100c6765bead849d568962ad1266cfc86ec
achala.kavinda <achalakavinda25r@gmail.com>
2016-08-20 11:35:32 +0530

    Customer + Retal System css/customer table created : customer ,loyality, cutomer rental

---
+++ database/migrations/2016_08_19_162938_create_customer_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers',function(Blueprint $table){
+                $table->increments('id');
+                $table->string('fname');
+                $table->string('sname');
+                $table->string('lname');
+                $table->string('otherName');
+                $table->integer('age');
+                $table->date('dob');
+                $table->string('number')->unique();
+                $table->string('nic')->unique();
+                $table->string('passport');
+                $table->string('address1');
+                $table->string('address2');
+                $table->string('loyalty');
+                $table->boolean('terminated');
+                $table->timestamps('created_date');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}
