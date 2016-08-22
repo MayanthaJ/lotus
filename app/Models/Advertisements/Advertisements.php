5c24f10f268ed68629b4019ea0a36b5ab15b418a
Sithira <sithiraac@gmail.com>
2016-08-22 07:04:53 +0530

    Fixed udana's files

--- app/Models/Advertisements/Advertisements.php
+++ app/Models/Advertisements/Advertisements.php
@@ -1,27 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models\Advertisements;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Advertisements extends Model
 {
-
-    protected $fillable =[
-
-        'name',
-
-        'type',
-
-        'file',
-
-        'created_at',
-
-        'updated_at'
-
-
-
-    ];
-
-
+    public $guarded = ['id'];
 }
